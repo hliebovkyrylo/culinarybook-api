49fc1f6a2fa3defbf2c8d89535c096063d757108
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-04-11 10:01:25 +0300

    slightly changed the password reset logic

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -15,6 +15,7 @@ authRoute.post('/sign-up', validate(SignUpSchema), authController.signUp);
 authRoute.post('/sign-in', validate(SignInSchema), authController.signIn);
 authRoute.post('/send-code', isAuth, authController.sendConfirmationCode);
 authRoute.post('/verify-email', isAuth, isCodeExprired, authController.verifyEmail);
+authRoute.post('/forgot-password', authController.forgotPassword);
 authRoute.post('/resent-code', isAuth, authController.resendConfirmationCode);
 authRoute.patch('/change-password', isAuth, validate(ChangePasswordSchema), authController.changePassword);
 authRoute.patch('/canReset-password', isAuth, isCodeExprired, authController.canResetPassword);
