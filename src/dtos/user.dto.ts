10af7264ef5b350e50f578a225c4f06959d29d3c
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-04-15 13:11:09 +0300

    improvement(user): added to the answer when receiving users the number of subscribers and recipes

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,7 +1,13 @@
 import { type User } from "@prisma/client";
 
-export type IUserDTO        = Omit<User, "password">;
-export type IUserPreviewDTO = Omit<User, "password" | "isVerified" | "canResetPassword" | "email" | "isPrivate">;
+export type IUserDTO = Omit<User, "password">;
+
+export type IUserPreviewDTO = Omit<User, "password" | "isVerified" | "canResetPassword" | "email" | "isPrivate"> & {
+  followerCount: number;
+  recipeCount  : number;
+};
+
+export type IUserFollowPreviewDTO = Omit<User, "password" | "isVerified" | "canResetPassword" | "email" | "isPrivate">;
 
 export class ProfileDTO implements IUserDTO {
   public id              : string;
@@ -9,12 +15,12 @@ export class ProfileDTO implements IUserDTO {
   public username        : string;
   public name            : string;
   public image           : string;
-  backgroundImage        : string;
+  public backgroundImage : string;
   public isVerified      : boolean;
   public canResetPassword: boolean;
   public isPrivate       : boolean;
 
-  constructor (data: IUserDTO) {
+  constructor(data: IUserDTO) {
     this.id               = data.id,
     this.email            = data.email,
     this.username         = data.username,
@@ -24,21 +30,41 @@ export class ProfileDTO implements IUserDTO {
     this.isVerified       = data.isVerified,
     this.canResetPassword = data.canResetPassword
     this.isPrivate        = data.isPrivate
-  };
-};
+  }
+}
 
 export class UserPreviewDTO implements IUserPreviewDTO {
-  public id             : string;
-  public username       : string;
-  public name           : string;
-  public image          : string;
+  public id: string;
+  public username: string;
+  public name: string;
+  public image: string;
+  public backgroundImage: string;
+  public followerCount: number;
+  public recipeCount: number;
+
+  constructor(data: IUserPreviewDTO) {
+    this.id = data.id,
+    this.username = data.username,
+    this.name = data.name,
+    this.image = data.image,
+    this.backgroundImage = data.backgroundImage
+    this.followerCount = data.followerCount;
+    this.recipeCount = data.recipeCount;
+  }
+}
+
+export class UserFollowPreviewDTO implements IUserFollowPreviewDTO {
+  public id: string;
+  public username: string;
+  public name: string;
+  public image: string;
   public backgroundImage: string;
 
-  constructor (data: IUserPreviewDTO) {
-    this.id              = data.id,
-    this.username        = data.username, 
-    this.name            = data.name,
-    this.image           = data.image,
+  constructor(data: IUserFollowPreviewDTO) {
+    this.id = data.id,
+    this.username = data.username,
+    this.name = data.name,
+    this.image = data.image,
     this.backgroundImage = data.backgroundImage
-  };
-};
+  }
+}
