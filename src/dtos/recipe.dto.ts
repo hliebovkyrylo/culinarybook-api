6cd4abbff5e79bd5a7897c944300df9425dba4e0
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-04-18 11:49:09 +0300

    improvement(recipe): added a field to apply the recipe background

--- src/dtos/recipe.dto.ts
+++ src/dtos/recipe.dto.ts
@@ -1,6 +1,6 @@
 import { Recipe, User } from "@prisma/client";
 
-export type IPreviewRecipeDTO = Omit<Recipe, "ingradients" | "isPublic" | "ownerId"> & { owner: User };
+export type IPreviewRecipeDTO = Omit<Recipe, "ingradients" | "isPublic" | "ownerId" | "applyBackground"> & { owner: User };
 
 export class RecipePreviewDTO implements IPreviewRecipeDTO {
   public id          : string;
