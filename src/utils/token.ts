ae65a3ac9d7c370820f9af6fadb83d9a1d83dbbb
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-06-24 14:16:06 +0300

    feat(auth): created a refresh token

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,7 +1,8 @@
-import jwt           from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-const JWT_SECRET     = process.env.JWT_SECRET as string;
-const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+const JWT_SECRET             = process.env.JWT_SECRET as string;
+const JWT_ACCESS_EXPIRES_IN  = process.env.JWT_ACCESS_EXPIRES_IN;
+const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN;
 
 interface IPayload {
   id: string;
@@ -14,10 +15,22 @@ export const createAccessToken = (id: string) => {
     },
     JWT_SECRET,
     {
-      expiresIn: JWT_EXPIRES_IN,
+      expiresIn: JWT_ACCESS_EXPIRES_IN,
     },
   )
 };
+
+export const createRefreshToken = (id: string) => {
+  return jwt.sign(
+    {
+      id
+    },
+    JWT_SECRET,
+    {
+      expiresIn: JWT_REFRESH_EXPIRES_IN,
+    }
+  )
+}
 
 export const verifyToken = (token: string) => {
   const payload = jwt.verify(token, JWT_SECRET) as IPayload;
