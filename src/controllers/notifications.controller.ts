7232b69ce93b1ed1e0a778f8f3ba2cb59b42da83
Kyrylo Hliebov <kirilglebow030@gmail.com>
2024-06-29 14:37:10 +0300

    fix(notification): fixed receiving notifications

--- src/controllers/notifications.controller.ts
+++ src/controllers/notifications.controller.ts
@@ -5,14 +5,14 @@ import { notificationService } from "../services/notification.service";
 class NotificationController {
   public async getAllMyNotifications(request: Request, response: Response) {
     const user = request.user as User;
-    const notifications = notificationService.getNotificationsByUserId(user.id);
+    const notifications = await notificationService.getNotificationsByUserId(user.id);
 
     response.send(notifications);
   };
 
   public async getAllMyUnreadedNotifications(request: Request, response: Response) {
     const user = request.user as User;
-    const unreadedNotifications = notificationService.getUnreadedNotifications(user.id);
+    const unreadedNotifications = await notificationService.getUnreadedNotifications(user.id);
 
     response.send(unreadedNotifications);
   }
